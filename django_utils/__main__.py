05f3afa1ee501b94724a8d92f26700b950fb07e6
Laurent-Alou Koné <laurent.alou.k@gmail.com>
2020-02-21 00:04:03 +0100

    feat: Add generation of config file

--- django_utils/__main__.py
+++ django_utils/__main__.py
@@ -20,10 +20,11 @@ DEFAULT_CONFIG_FILE = "/etc/django_utils/config"
 
 def check_config_file(args):
     if not os.path.exists(args.config_file):
-        print("<WARNING> Configuration file not found: {args.config_file}")
-        print("<WARNING> Only argument and default config will be used")
+        print(f"<WARNING> Configuration file not found: {args.config_file}")
+        print(" ** Only argument and default config will be used")
         if args.config_file != DEFAULT_CONFIG_FILE:
-            print("<WARNING> You can use --config-file option for another config file")
+            print(" ** You can use --config-file option for choose another config file")
+            print(" ** Or use --new-config-file to generate a new default config file")
 
 def args_parse(args_liner):
     """Argument Parse/Check"""
@@ -31,26 +32,35 @@ def args_parse(args_liner):
     parser = argparse.ArgumentParser()
     parser.add_argument('env', help='Environment (dev|prd)', choices=['dev', 'prd'])
 
-    parser.add_argument('-f', '--config-file', help='Config file', default='/etc/django_utils/config')
+    parser.add_argument('-f', '--config-file',
+        help='Config file, default: {DEFAULT_CONFIG_FILE}',
+        default=DEFAULT_CONFIG_FILE)
+    parser.add_argument('--new-config-file', help='Generate a default config file')
     parser.add_argument('-s', '--start', action="store_true", help='Start web server')
     parser.add_argument('-k', '--stop', action="store_true", help='Stop web server')
-    parser.add_argument('-c', '--check', action="store_true", help='Check web server status [Start if down]')
+    parser.add_argument('-c', '--check',
+        action="store_true",
+        help='Check web server status [Start if down]')
     parser.add_argument('--db-restart', action="store_true", help='Database Restart')
 
     parser.add_argument('--virtual-env', action="store_true", help='Active Virtual ENV')
     parser.add_argument('--new-env', action="store_true", help='New Virtual ENV')
 
-    parser.add_argument('--ssh', action="store_true", help='SSH backup/restore')
-    parser.add_argument('--s3', action="store_true", help='S3 backup/restore')
-
     parser.add_argument('--clean-mig-file', action="store_true", help='Clean app migration files')
 
     parser.add_argument('--db-backup', action="store_true", help='Database Backup')
-    parser.add_argument('--db-restore', action="store_true", help='Database Restore')
-    parser.add_argument('--dump-file', required=re.search('--db-restore', args_liner), help='Dump File to Restore [.dump(.xz)]')
     parser.add_argument('--media-backup', action="store_true", help='Media Backup')
+    parser.add_argument('--db-restore', action="store_true", help='Database Restore')
     parser.add_argument('--media-restore', action="store_true", help='Media Restore')
-    parser.add_argument('--media-file', required='--media-restore' in sys.argv, help='Media File to Restore [.tar(.xz)]')
+
+    parser.add_argument('--dump-file',
+        required=re.search('--db-restore', args_liner),
+        help='Dump File to Restore [.dump(.xz)]')
+    parser.add_argument('--media-file',
+        required='--media-restore' in sys.argv,
+        help='Media File to Restore [.tar(.xz)]')
+    parser.add_argument('--ssh', action="store_true", help='SSH backup/restore')
+    parser.add_argument('--s3', action="store_true", help='S3 backup/restore')
 
     parser.add_argument('--no-log', action="store_true", help='No output file log')
     parser.add_argument('--debug', action="store_true", help='Debug Mode')
@@ -66,19 +76,30 @@ def main():
     # Argument parse
     args_liner = ' '.join(sys.argv)
     args = args_parse(args_liner)
-    # Check default config
-    check_config_file(args)
+
+    if not args.new_config_file:
+        # Check default config
+        check_config_file(args)
     # Config parse
     cfg = Config.build(args)
+    if args.new_config_file:
+
+    # Create new config file
+        Config.write(cfg, args.new_config_file)
     if args.check:
-        args.start = True 
-    # Change directory to project path
-    os.chdir(cfg["global"]["project_path"])
-    # Init Script logger #
+        args.start = True
+
+    # Init Script logger
     log_file = logger_init(args, cfg)
-    logger.info(f'[script] o-> Start (Env:{args.env}) <-o')
-    # File Extension + Existing Path Check #
-    if not args.db_restore and not args.media_restore: 
+    logger.info(f'[script] o-> Start (Env:{args.env.capitalize()}) <-o')
+
+    # Check and Change directory to project path
+    project_path = cfg["global"]["project_path"]
+    check_path_o_file(project_path, 'Project', create_path=False)
+    os.chdir(project_path)
+
+    # Check existing media path
+    if not re.search("-restore", args_liner):
         check_path_o_file(cfg["django"]["media_path"], 'Media', create_path=False)
 
     if args.virtual_env:
